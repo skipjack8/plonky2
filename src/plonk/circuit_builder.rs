3fabdeb20bd79945fcda0c3f016d76da97e30380
Nicholas Ward <npward@berkeley.edu>
2021-10-19 15:39:39 -0700

    fix: run all U32SubtractionGate generators

--- src/plonk/circuit_builder.rs
+++ src/plonk/circuit_builder.rs
@@ -519,8 +519,7 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
         let mut timing = TimingTree::new("preprocess", Level::Trace);
         let start = Instant::now();
 
-        self.fill_arithmetic_gates();
-        self.fill_switch_gates();
+        self.fill_batched_gates();
 
         // Hash the public inputs, and route them to a `PublicInputGate` which will enforce that
         // those hash wires match the claimed public inputs.
@@ -926,5 +925,12 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
                 self.connect(zero, wire_input_borrow);
             }
         }
+    }
+
+    fn fill_batched_gates(&mut self) {
+        self.fill_arithmetic_gates();
+        self.fill_switch_gates();
+        self.fill_u32_arithmetic_gates();
+        self.fill_u32_subtraction_gates();
     }
 }
